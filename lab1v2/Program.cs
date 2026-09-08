5997afcecd9d69d45e58c4fcc3d3f3a3fdaf18f9
Bziton <bzita.o.m_ipz24@rcit.ukr.education>
2026-09-08 17:46:19 +0300

    Замінено lab1v5 на lab1v2

---
+++ lab1v2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _year;
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                if (value >= 1886 && value <= 2026)
+                {
+                    _year = value;
+                }
+            }
+        }
+
+        public Car(string brand, string model, int year)
+        {
+            _brand = brand;
+            _model = model;
+            _year = year;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"Автомобіль {_brand} {_model} ({_year} року) вирушив у дорогу.");
+        }
+    }
+
+    class Cars
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW", "M5", 2021);
+            car1.Drive();
+
+            Car car2 = new Car("Audi", "RS6", 2022);
+            car2.Drive();
+        }
+    }
+}
